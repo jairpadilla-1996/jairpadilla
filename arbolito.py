1313a2c20af2311076c3fdf600dee46de4587fab
jair <jairgotze.17@gmail.com>
2025-04-01 19:26:36 -0500

    Add files via upload

---
+++ arbolito.py
@@ -0,0 +1,33 @@
+import turtle
+t=turtle.Turtle()
+t.screen.bgcolor('white')
+t.pensize(2)
+t.left(90)
+t.backward(100)
+t.speed(200)
+t.shape('turtle')
+def tree(i):
+    if i<10:
+        return
+    else:
+        t.forward(i)
+        t.color('red')
+        t.circle(2)
+        t.color('brown')
+        t.left(30)
+        tree(3*i/4)
+        t.right(60)
+        tree(3*i/4)
+        t.left(30)
+        t.backward(i)
+tree(100)
+turtle.done()        
+        
+        
+        
+        
+        
+        
+        
+        
+        
